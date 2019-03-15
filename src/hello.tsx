87752e73bd8962084419970b2eb7bc50571665da
freewind <nowindlee@gmail.com>
2019-03-15 15:07:14 +0800

    use different colors

--- src/hello.tsx
+++ src/hello.tsx
@@ -14,21 +14,22 @@ class Hello extends React.Component {
     ]
 
     const layout2: Layout[] = [
-      {i: 'm', x: 0, y: 0, w: 4, h: 2},
-      {i: 'n', x: 1, y: 0, w: 4, h: 2},
+      {i: 'm', x: 0, y: 0, w: 1, h: 1},
+      {i: 'n', x: 1, y: 0, w: 1, h: 2},
     ]
 
     return <div>
       <h1>Hello React</h1>
       <GridLayout className='layout' layout={layout1} cols={12} rowHeight={30} width={1200}>
-        <div key='a'>A</div>
-        <div key='b'>
-          <GridLayout className='layout' layout={layout2} cols={4} rowHeight={30} width={200} containerPadding={[0, 0]}>
-            <div key='m'>M</div>
-            <div key='n'>N</div>
+        <div key='a' className='outer'>A</div>
+        <div key='b' className='outer'>
+          <GridLayout className='layout' layout={layout2} cols={12} rowHeight={30} width={1200}
+                      containerPadding={[0, 0]}>
+            <div key='m' className='inner'>M</div>
+            <div key='n' className='inner'>N</div>
           </GridLayout>
         </div>
-        <div key='c'>C</div>
+        <div key='c' className='outer'>C</div>
       </GridLayout>
     </div>
   }
